324b2c8cf76588fcc9c3f71f9fc9e4266a5a1580
abdusattorovJaloliddin <jaloliddinabdusattorov2005@gmail.com>
2025-08-01 12:37:47 +0500

    as

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 
 function ContactPage() {
   const [name, setName] = useState('');
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import  { useState, useEffect } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
 import { MdDelete, MdEdit } from "react-icons/md";
